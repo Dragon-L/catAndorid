fd6bf5eb803b859d3e4d9e123bdff089122c1dcf
Guolong <glliao@thoughtworks.com>
2017-11-08 15:53:46 +0800

    introduce butterknife

--- app/src/main/java/com/example/glliao/catandorid/activity/HomeActivity.kt
+++ app/src/main/java/com/example/glliao/catandorid/activity/HomeActivity.kt
@@ -4,24 +4,29 @@ import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.example.glliao.catandorid.R
 import com.example.glliao.catandorid.adapter.BannerAdapter
 import com.example.glliao.catandorid.fragment.MyCatFragment
 import com.example.glliao.catandorid.fragment.NearByFragment
 
 class HomeActivity : AppCompatActivity() {
-    private lateinit var mViewPager: ViewPager
+
+    @BindView(R.id.viewPager)
+    lateinit var mViewPager: ViewPager
+
     private val mImageResIds = arrayListOf<Int>(R.mipmap.catas, R.mipmap.catgrey, R.mipmap.cats, R.mipmap.catwhite)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        ButterKnife.bind(this)
         initBanner()
         setupFragment()
     }
 
     private fun initBanner() {
-        mViewPager = findViewById(R.id.viewPager)
         var imageList = ArrayList<ImageView>()
 
         mImageResIds.forEach {
--- app/src/main/java/com/example/glliao/catandorid/activity/MainActivity.kt
+++ app/src/main/java/com/example/glliao/catandorid/activity/MainActivity.kt
@@ -6,14 +6,22 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.example.glliao.catandorid.R
 import com.example.glliao.catandorid.presenter.LoginContract
 import com.example.glliao.catandorid.presenter.LoginPresenter
 
 class MainActivity : AppCompatActivity(), LoginContract.View {
-    private lateinit var mEditUserName : EditText
-    private lateinit var mEditPassword : EditText
-    private lateinit var mBtnLogin : Button
+
+    @BindView(R.id.button_login)
+    lateinit var mBtnLogin: Button
+
+    @BindView(R.id.user_name)
+    lateinit var mEditUserName: EditText
+
+    @BindView(R.id.password)
+    lateinit var mEditPassword: EditText
 
     private val TAG = "LoginActivity"
     private val presenter = LoginPresenter(this)
@@ -22,15 +30,7 @@ class MainActivity : AppCompatActivity(), LoginContract.View {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onStart")
         setContentView(R.layout.activity_main)
-        mBtnLogin = findViewById(R.id.button_login)
-        mEditUserName = findViewById(R.id.user_name)
-        mEditPassword = findViewById(R.id.password)
-        mBtnLogin.setOnClickListener{
-            var userName = mEditUserName.editableText.toString()
-            var password = mEditPassword.editableText.toString()
-            Log.i("cat", "Button clicked!")
-            Log.i("cat", userName + password)
-        }
+        ButterKnife.bind(this)
 
         mBtnLogin.setOnClickListener {
             presenter.startLogin(mEditUserName.text.toString(), mEditPassword.text.toString())
@@ -42,25 +42,5 @@ class MainActivity : AppCompatActivity(), LoginContract.View {
         Log.d(TAG, "login successful!!!!")
         startActivity(Intent(this, HomeActivity::class.java))
         finish()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestory")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG,"onStop")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
     }
 }
